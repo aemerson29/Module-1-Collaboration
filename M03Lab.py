148b5ffab017c1e3354fd731a7967b09bca9b851
Alan Emerson <Aemerson29@ivytech.edu>
2025-04-06 12:28:29 -0400

    testing

--- M03Lab.py
+++ M03Lab.py
@@ -13,21 +13,25 @@ class Automobile(Vehicle):
         super().__init__(year, brand, module)
         self.doors = doors
         self.roof = roof
+        
     #prints out all the data in a more readable form
     def Display_car(self):
         print("Year: ", self.year, "\nBrand: ", self.brand, "\nModule: ", self.module, "\nNumber of doors:", self.doors, "\nType of roof:", self.roof)
 
-
+#creates a car
 def create_car():
+        #first gathers all data needed
         year = str(input("What year was it made?"))
         brand = str(input("What brand is it?"))
         module = str(input("What module is it?"))
         doors = str(input("How many doors does it have?"))
         roof = str(input("What type of roof does it have?"))
 
+        #then creates an Automobile using the data collected
         car = Automobile(year, brand, module, doors, roof)
+        #returns newly created Automobile
         return car
 
+#test run
 car01 = create_car()
-
 car01.Display_car()
